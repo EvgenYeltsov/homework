075f92090e158a98dc0e75b469b29eae8ccde333
kanclerrVRN <kanclerr@gmail.com>
2021-09-25 12:51:46 +0300

    initial

---
+++ 1_1_2.py
@@ -0,0 +1,8 @@
+# Task 1_1_2.
+# Using parameters sep, end.
+print("Hello", "world", sep='___', end='!\n')
+# Print Hello world
+print("Hello "
+      "world")
+# Using parameter sep.
+print("Hello", "world", sep='\t')
