34e1f2461c2f3e197c91fb7894196889a726e612
kanclerrVRN <kanclerr@gmail.com>
2021-10-01 15:40:26 +0300

    использовал функцию lower

--- Task5/1_5_3.py
+++ Task5/1_5_3.py
@@ -1,7 +1,16 @@
 # Task 1_5_3. The name check.
+# my_name = 'evgeny'
+# str_1 = input("Please write your name:")
+# if str_1 == my_name or str_1 == my_name.title():
+#    print("Welcome", str_1)
+# else:
+#    print("Access denied")
+
+# с использованием функции lower. Дает вводить любые символы имени в любом регистре и получать на выходе True
 my_name = 'evgeny'
 str_1 = input("Please write your name:")
-if str_1 == my_name or str_1 == my_name.title():
-   print("Welcome", str_1)
+str_1 = str_1.lower()
+if str_1 == my_name:
+    print("Welcome", str_1)
 else:
-   print("Access denied")
+    print("Access denied")
